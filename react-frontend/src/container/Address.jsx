051e905777dab4ac4e28e8210bddc6b2b4c01c0e
Sumanta Bhui <39363349+bhuisumanta@users.noreply.github.com>
2022-05-05 09:00:38 +0530

    Revert "Add profiler"

--- react-frontend/src/container/Address.jsx
+++ react-frontend/src/container/Address.jsx
@@ -1,22 +1,9 @@
-import React, { Profiler } from "react";
+import React from "react";
 import TextHighlight from "../common/TextHighlight";
 
 function Address({ data, schema, qLength }) {
-  function onRenderCallback(
-    id, // the "id" prop of the Profiler tree that has just committed
-    phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
-    actualDuration, // time spent rendering the committed update
-    baseDuration, // estimated time to render the entire subtree without memoization
-    startTime, // when React began rendering this update
-    commitTime, // when React committed this update
-    interactions // the Set of interactions belonging to this update
-  ) {
-    // Aggregate or log render timings...
-    console.log(id, phase, actualDuration, baseDuration, startTime, commitTime, interactions);
-  }
   return (
     <>
-    <Profiler id="Navigation" onRender={onRenderCallback}>
       <p>
         <TextHighlight
           text={data.sentence}
@@ -44,7 +31,6 @@ function Address({ data, schema, qLength }) {
           />
         </small>
       </div>
-      </Profiler>
     </>
   );
 }
